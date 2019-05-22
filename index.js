481d8d35a7b2142ddaf311f999226e83f516492f
Ubuntu <imnotminh@gmail.com>
2019-05-22 16:27:12 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+// Code your solution 
+function findMatching(drivers,string){
+    return drivers.filter(function(x){
+        return x.toLowerCase() === string.toLowerCase()
+    })
+}
+
+function fuzzyMatch(drivers,string){
+    let size = string.length
+    return drivers.filter(function(x){
+        return x.slice(0,size).toLowerCase() === string.toLowerCase()
+    })
+}
+
+function matchName(drivers,string){
+    return drivers.filter(function(x){
+        return x.name === string
+    })
+}
