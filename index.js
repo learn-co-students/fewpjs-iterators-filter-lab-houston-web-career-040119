cb17588b054f87788bc614100f0bf42fc53e72c4
shawn0527 <shawnren527@gmail.com>
2019-05-21 22:08:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(array, word) {
+    return array.filter(element => element.toLowerCase() === word.toLowerCase())
+}
+
+function fuzzyMatch(array, word) {
+    return array.filter(element => element.slice(0, word.length) === word)
+}
+
+function matchName(array, name) {
+    return array.filter(obj => obj.name === name)
+}
