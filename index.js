7cc783d9fb68f5bfe4fdbb1580b2b02150d7734e
PYN <yxp7531540@gmail.com>
2019-05-22 13:42:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase === name.toLowerCase)
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => {
+        let lastIndex = driver.length - 1
+        let middleIndex = lastIndex.floor()
+        let beginningPartName = driver.slice(0, middleIndex - 1)
+        return beginningPartName.includes(letters)
+    })
+}
